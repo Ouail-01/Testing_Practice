4be4eac37bbae5ee4a3b649453057b491499a803
Ouail-01 <ouaillaarabi@gmail.com>
2023-01-25 21:51:58 +0100

    Updating

--- Testing/test1.test.js
+++ Testing/test1.test.js
@@ -1,10 +1,7 @@
-/* eslint-disable */
-const stringLength = require("../test1");
+const stringLength = require('../test1');
 
-test("return the exact characters in a string", () => {
-  expect(stringLength("testing")).toEqual(7);
-  expect(stringLength("Js")).toEqual(2);
-  expect(stringLength("")).toEqual(0);
+test('return the exact characters in a string', () => {
+  expect(stringLength('testing')).toEqual(7);
+  expect(stringLength('Js')).toEqual(2);
+  expect(stringLength('')).toEqual(0);
 });
-
-// const stringLength = (string) => string.length;
